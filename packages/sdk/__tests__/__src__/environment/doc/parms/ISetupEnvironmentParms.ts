a02bcf9a3db0f727b74182ca50c13b7d5689c930
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-05-04 17:09:49 +0000

    update license heder
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- packages/sdk/__tests__/__src__/environment/doc/parms/ISetupEnvironmentParms.ts
+++ packages/sdk/__tests__/__src__/environment/doc/parms/ISetupEnvironmentParms.ts
@@ -1,13 +1,13 @@
-/*
-* This program and the accompanying materials are made available under the terms of the *
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
-* https://www.eclipse.org/legal/epl-v20.html                                      *
-*                                                                                 *
-* SPDX-License-Identifier: EPL-2.0                                                *
-*                                                                                 *
-* Copyright Contributors to the Zowe Project.                                     *
-*                                                                                 *
-*/
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
 
 /**
  * Parameters used to setup your isolated test directory. Jest allows parallel test execution and many of the
--- packages/vsce/src/utils/profileManagement.ts
+++ packages/vsce/src/utils/profileManagement.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-/*
-* This program and the accompanying materials are made available under the terms of the
-* Eclipse Public License v2.0 which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Copyright Contributors to the Zowe Project.
-*
-*/
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
 
 import { IDeleteProfile, IProfileLoaded, IProfileUpdated, ISaveProfile, IUpdateProfile } from "@zowe/imperative";
 import { ZoweVsCodeExtension } from "@zowe/zowe-explorer-api";
