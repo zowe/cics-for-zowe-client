732dd1758cf6bf6017ec809b8241e252a89bff3d
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-08-09 17:21:37 +0000

    test001
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- .github/release.config.js
+++ .github/release.config.js
@@ -2,6 +2,7 @@ module.exports = {
     branches: [
         {
             name: "main",
+            channel: "latest",
             level: "minor",
             devDependencies: {
                 "@zowe/imperative": "zowe-v2-lts",
@@ -10,24 +11,78 @@ module.exports = {
         },
         {
             name: "zowe-v1-lts",
+            channel: "zowe-v1-lts",
             level: "patch",
             devDependencies: {
                 "@zowe/imperative": "zowe-v1-lts",
                 "@zowe/zowe-explorer-api": "zowe-v1-lts",
             }
+        },
+        {
+            name: "next",
+            channel: "next",
+            level: "major",
+            prerelease: true,
+            devDependencies: {
+                "@zowe/imperative": "next",
+                "@zowe/zowe-explorer-api": "next",
+            }
         }
     ],
     plugins: [
-        "@octorelease/changelog",
-        ["@octorelease/npm", {
-            aliasTags: {
-                latest: ["zowe-v2-lts"]
+        [
+            "@octorelease/changelog",
+            {
+                displayNames: {
+                    "cli": "IBM CICS Plug-in for Zowe CLI",
+                    "sdk": "IBM CICS for Zowe SDK",
+                    "vsce": "IBM CICS Extension for Zowe Explorer",
+                },
+                headerLine: "## Recent Changes",
             },
-            pruneShrinkwrap: false
-        }],
-        ["@octorelease/github", {
-            checkPrLabels: true
-        }],
-        "@octorelease/git"
+        ],
+        [
+            "@octorelease/lerna",
+            {
+                // Use Lerna only for versioning and publish packages independently
+                npmPublish: false,
+            },
+        ],
+        [
+            "@octorelease/npm",
+            {
+                $cwd: "packages/sdk",
+                aliasTags: {
+                    "latest": ["zowe-v2-lts"],
+                },
+                npmPublish: false,
+                tarballDir: "dist",
+            },
+            {
+                $cwd: "packages/cli",
+                aliasTags: {
+                    "latest": ["zowe-v2-lts"],
+                },
+                npmPublish: false,
+                tarballDir: "dist",
+            },
+        ],
+        [
+            "@octorelease/vsce",
+            {
+                $cwd: "packages/vsce",
+                ovsxPublish: false,
+                vscePublish: false,
+                vsixDir: "dist",
+            },
+        ],
+        [
+            "@octorelease/github",
+            {
+                assets: ["dist/*.tgz", "dist/*.vsix"],
+                draftRelease: true,
+            },
+        ],
+        "@octorelease/git",
     ]
 };
