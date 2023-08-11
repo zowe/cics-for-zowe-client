0badf8c7d7d8772736e4dc8c9b448dd724238b52
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-08-11 14:38:08 +0000

    make test a release branch
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- .github/release.config.js
+++ .github/release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     branches: [
         {
-            name: "main",
+            name: "test",
             channel: "latest",
             // level: "minor",
             devDependencies: {
