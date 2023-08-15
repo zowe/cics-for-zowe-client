ba16917f81e6d2df38ccc84d2a6178ff0a4f793a
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-08-15 13:10:53 +0000

    rename root to cli plugin name
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- scripts/rewriteShrinkwrap.js
+++ scripts/rewriteShrinkwrap.js
@@ -10,47 +10,61 @@
 */
 
 const fs = require("fs");
-const cp = require("child_process");
-// const chalk = require("chalk");
-// const getLockfile = require("npm-lockfile/getLockfile");
+// const cp = require("child_process");
+const chalk = require("chalk");
+const getLockfile = require("npm-lockfile/getLockfile");
 
 const rootDir = __dirname + "/../";
 const cliDir = rootDir + "packages/cli/";
-// const rootPackageLockFile = rootDir + "package-lock.json";
-// const rootShrinkwrapFile = rootDir + "npm-shrinkwrap.json";
+const rootPackageLockFile = rootDir + "package-lock.json";
+const rootShrinkwrapFile = rootDir + "npm-shrinkwrap.json";
 const cliShrinkwrapFile = cliDir + "npm-shrinkwrap.json";
 
-// if (!fs.existsSync(rootShrinkwrapFile)) {
-//   if (fs.existsSync(rootPackageLockFile)) {
-//     fs.writeFileSync(rootShrinkwrapFile, JSON.stringify(JSON.parse(fs.readFileSync(rootPackageLockFile, "utf-8")), null, 2));
-//   } else {
-//     throw "No package-lock.json or npm-shinkwrap.json found.";
-//   }
-// }
-
-// // Remove "file:" links from shrinkwrap
-// const shrinkwrap = JSON.parse(fs.readFileSync(rootShrinkwrapFile, "utf-8"));
-// for (const [k, v] of Object.entries(shrinkwrap.packages)) {
-//     if (v.link) {
-//         // delete shrinkwrap.packages[k];
-//     }
-// }
-// fs.writeFileSync(cliShrinkwrapFile, JSON.stringify(shrinkwrap, null, 2));
-
-const pkgA = rootDir + "package.json";
-const pkgB = rootDir + "package.json_";
-try {
-  // Mimic non-workspaces monorepo
-  fs.renameSync(pkgA, pkgB);
-
-  cp.execSync("npm shrinkwrap", {cwd: cliDir});
-
-  const shrinkwrap = JSON.parse(fs.readFileSync(cliShrinkwrapFile, "utf-8"));
-  shrinkwrap.lockfileVersion = 2;
-  fs.writeFileSync(cliShrinkwrapFile, JSON.stringify(shrinkwrap, null, 2));
-
-  cp.execSync("npm i ../sdk --install-links --package-lock-only", {cwd: cliDir});
-} finally{
-  // revert back to workspaces monorepo
-  fs.renameSync(pkgB, pkgA);
+if (!fs.existsSync(rootShrinkwrapFile)) {
+  if (fs.existsSync(rootPackageLockFile)) {
+    fs.writeFileSync(rootShrinkwrapFile, JSON.stringify(JSON.parse(fs.readFileSync(rootPackageLockFile, "utf-8")), null, 2));
+  } else {
+    throw "No package-lock.json or npm-shinkwrap.json found.";
+  }
 }
+
+// Remove "file:" links from shrinkwrap
+const shrinkwrap = JSON.parse(fs.readFileSync(rootShrinkwrapFile, "utf-8"));
+for (const [k, v] of Object.entries(shrinkwrap.packages)) {
+    if (v.link) {
+        delete shrinkwrap.packages[k];
+    }
+}
+fs.writeFileSync(cliShrinkwrapFile, JSON.stringify(shrinkwrap, null, 2));
+
+// Build deduped shrinkwrap for @zowe/cics-for-zowe-cli
+const zoweRegistry = require("../lerna.json").command.publish.registry;
+getLockfile(cliShrinkwrapFile, undefined, { "@zowe:registry": zoweRegistry })
+.then((lockfile) => fs.writeFileSync(cliShrinkwrapFile, lockfile))
+.then(() => console.log(chalk.green("Lockfile contents written!")))
+.catch((err) => {
+  if (err.statusCode !== 404) {
+    console.error(err);
+    process.exit(1);
+  } else {
+    console.log(chalk.green("Lockfile contents written!"));
+  }
+});
+
+// const pkgA = rootDir + "package.json";
+// const pkgB = rootDir + "package.json_";
+// try {
+//   // Mimic non-workspaces monorepo
+//   fs.renameSync(pkgA, pkgB);
+
+//   cp.execSync("npm shrinkwrap", {cwd: cliDir});
+
+//   const shrinkwrap = JSON.parse(fs.readFileSync(cliShrinkwrapFile, "utf-8"));
+//   shrinkwrap.lockfileVersion = 2;
+//   fs.writeFileSync(cliShrinkwrapFile, JSON.stringify(shrinkwrap, null, 2));
+
+//   cp.execSync("npm i ../sdk --install-links --package-lock-only", {cwd: cliDir});
+// } finally{
+//   // revert back to workspaces monorepo
+//   fs.renameSync(pkgB, pkgA);
+// }
