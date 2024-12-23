727f9d37a8feca1a117ca8debc8c56ed6879e476
Chinmay Das <chinmay.das1@ibm.com>
2024-12-23 19:57:03 +0530

    lint corrections
    Signed-off-by: Chinmay Das <chinmay.das1@ibm.com>

--- packages/vsce/src/commands/manageSessionCommand.ts
+++ packages/vsce/src/commands/manageSessionCommand.ts
@@ -13,5 +13,5 @@ import { commands, ProgressLocation, TreeView, window } from "vscode";
 import { CICSTree } from "../trees/CICSTree";
 
 export function getManageSessionCommand(tree: CICSTree, treeview: TreeView<any>) {
-    return commands.registerCommand("cics-extension-for-zowe.manageSession", async (node) => tree.manageProfile(treeview, node));
+  return commands.registerCommand("cics-extension-for-zowe.manageSession", async (node) => tree.manageProfile(treeview, node));
 }
