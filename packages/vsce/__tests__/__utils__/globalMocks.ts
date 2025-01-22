30a9b70600e6a3643b084d5130b0106ad458a382
Rushabh Sojitra <Rushabh.Sojitra@ibm.com>
2025-01-22 15:53:54 +0530

    Unit test updated for LocalFile and refactored test files and moved common items to globalMocks
    Signed-off-by: Rushabh Sojitra <Rushabh.Sojitra@ibm.com>

---
+++ packages/vsce/__tests__/__utils__/globalMocks.ts
@@ -0,0 +1,45 @@
+import { ICMCIApiResponse } from "@zowe/cics-for-zowe-sdk";
+import { imperative } from "@zowe/zowe-explorer-api";
+import * as filterUtils from "../../src/utils/filterUtils";
+
+export const zoweSdkMock = require("@zowe/cics-for-zowe-sdk");
+export const toEscapedCriteriaString = jest.spyOn(filterUtils, "toEscapedCriteriaString");
+export const getResourceMock = jest.spyOn(zoweSdkMock, "getResource");
+
+export const imperativeSession = new imperative.Session({
+  user: "user",
+  password: "pwd",
+  hostname: "hostname",
+  protocol: "https",
+  type: "basic",
+  rejectUnauthorized: false,
+});
+
+const CICSSessionTreeMock = {
+  session: imperativeSession,
+};
+
+export const cicsRegionTreeMock = {
+  parentSession: CICSSessionTreeMock,
+  getRegionName: () => "IYK2ZXXX",
+  parentPlex: {
+    getPlexName: () => "PLEXX",
+  },
+};
+
+export const ICMCIApiResponseMock: ICMCIApiResponse = {
+    response: {
+      resultsummary: { api_response1: "1024", api_response2: "0", recordcount: "0", displayed_recordcount: "0" },
+      records: {},
+    },
+};
+  
+export function getDummyTreeResources(resourceName: string, defaultCriteria: string, responseRecords: string, iconPath?: string) {
+    return {
+        iconPath: iconPath ? "/icon/path" : iconPath,
+        resourceName:  resourceName,
+        defaultCriteria: defaultCriteria,
+        responseRecords: responseRecords
+    };
+}
+
