6564000f4c74500d428c237e68451db9f8ec8d46
Rushabh Sojitra <Rushabh.Sojitra@ibm.com>
2025-04-17 20:09:45 +0530

    css changed
    Signed-off-by: Rushabh Sojitra <Rushabh.Sojitra@ibm.com>

--- packages/vsce/src/webviews/src/views/toggle-resources/TogglePanel.tsx
+++ packages/vsce/src/webviews/src/views/toggle-resources/TogglePanel.tsx
@@ -1,6 +1,5 @@
 import { VSCodePanels } from "@vscode/webview-ui-toolkit/react";
 import CheckBox from "../../components/CheckBox";
-import PanelTab from "../../components/PanelTab";
 import PanelView from "../../components/PanelView";
 
 interface metas {
@@ -26,33 +25,27 @@ const getImg = (item: string) => {
 };
 
 export default function ToggleResources({ items }: props) {
-  const tabProps = [
-    {
-      id: "id1",
-      children: "Resources",
-    },
-  ];
   return (
-    <VSCodePanels>
-      {tabProps.map((val) => (
-        <PanelTab id={val.id} children={val.children}></PanelTab>
-      ))}
-      {items.map((val) => (
-        <PanelView
-          id={val.resourceName}
-          styles="set-margin"
-          children={
-            <CheckBox
-              label={val.humanReadableName}
-              checked={val.visible}
-              onchange={(e) => {
-                val.visible = (e.target as HTMLInputElement).checked;
-              }}
-              imgPath={getImg(val.resourceName)}
-            ></CheckBox>
-          }
-        ></PanelView>
-      ))}
-    </VSCodePanels>
+    <>
+      <VSCodePanels className="padding-top">
+        {items.map((val) => (
+          <PanelView
+            id={val.resourceName}
+            styles="set-margin"
+            children={
+              <CheckBox
+                styles="checkbox-style"
+                label={val.humanReadableName}
+                checked={val.visible}
+                onchange={(e) => {
+                  val.visible = (e.target as HTMLInputElement).checked;
+                }}
+                imgPath={getImg(val.resourceName)}
+              ></CheckBox>
+            }
+          ></PanelView>
+        ))}
+      </VSCodePanels>
+    </>
   );
 }
