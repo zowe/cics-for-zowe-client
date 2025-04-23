de843858aa71850d20b725701834589e2a2f0b92
Rushabh Sojitra <Rushabh.Sojitra@ibm.com>
2025-04-23 16:40:05 +0530

    updated wiremock file
    Signed-off-by: Rushabh Sojitra <Rushabh.Sojitra@ibm.com>

--- packages/vsce/__tests__/__e2e__/resources/start-wiremock.sh
+++ packages/vsce/__tests__/__e2e__/resources/start-wiremock.sh
@@ -1,8 +1,13 @@
+if [ -z "$1" ]; then
+  echo "No directory provided. Please provide a directory for WireMock mappings."
+  exit 1
+fi
+
 docker run -it --rm -d \
     -p 8080:8080 \
     --name wiremock \
     -v "$1"
-    wiremock/wiremock:3.10.0
+    wiremock/wiremock:latest
 
 sleep 1
 
