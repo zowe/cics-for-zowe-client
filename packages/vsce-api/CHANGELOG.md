542a431ebf23afcb58ac8fd4b0003fd39c9a2146
Andrew Twydell <andrew.twydell@ibm.com>
2025-06-19 19:51:36 +0100

    add vsce-api package
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

---
+++ packages/vsce-api/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to the IBM CICS for Zowe Explorer API will be documented in this file.
+
+## Recent Changes
+
+- Initial creation of IBM CICS for Zowe Explorer API
---
+++ packages/vsce-api/src/resources/ResourceTypes.ts
@@ -0,0 +1,24 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+export enum ResourceTypes {
+  CICSLocalFile = "CICSLocalFile",
+  CICSLocalTransaction = "CICSLocalTransaction",
+  CICSProgram = "CICSProgram",
+  CICSTCPIPService = "CICSTCPIPService",
+  CICSLibrary = "CICSLibrary",
+  CICSURIMap = "CICSURIMap",
+  CICSTask = "CICSTask",
+  CICSPipeline = "CICSPipeline",
+  CICSWebService = "CICSWebService",
+}
+
+export const SupportedResourceTypes: ResourceTypes[] = Object.values(ResourceTypes);
