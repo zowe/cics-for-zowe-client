af93c2ee9c26ab824c15aa1320c00c9fdf0ea4e1
Andrew Twydell <andrew.twydell@ibm.com>
2025-07-01 23:18:08 +0100

    add extension test to get API returned
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/src/webviews/resource-inspector-panel/ResourceInspector.tsx
+++ packages/vsce/src/webviews/resource-inspector-panel/ResourceInspector.tsx
@@ -71,6 +71,7 @@ const ResourceInspector = () => {
           <th id="th-1" className="header-cell-1 padding-left-10">
             <div className="div-display-1">{resourceInfo?.name ?? "..."}</div>
             <div className="div-display-1 div-display-2">
+              {/* @ts-ignore */}
               {resourceInfo?.resourceName ?? "..."}: {resourceInfo ? (resourceInfo?.resource.status || resourceInfo?.resource.enablestatus) : "..."}
             </div>
           </th>
