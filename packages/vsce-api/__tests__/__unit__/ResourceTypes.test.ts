0376736385acd969c09021954650c75621c78248
Andrew Twydell <andrew.twydell@ibm.com>
2025-07-11 13:53:30 +0100

    adds vsce-api package, unused
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

---
+++ packages/vsce-api/__tests__/__unit__/ResourceTypes.test.ts
@@ -0,0 +1,33 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { ResourceTypes, SupportedResourceTypes } from "../../src/resources/ResourceTypes";
+
+describe("ResourceType tests", () => {
+  it("should return list of supported resources", () => {
+    expect(SupportedResourceTypes).toEqual([
+      "CICSLocalFile",
+      "CICSLocalTransaction",
+      "CICSProgram",
+      "CICSTCPIPService",
+      "CICSLibrary",
+      "CICSURIMap",
+      "CICSTask",
+      "CICSPipeline",
+      "CICSWebService",
+    ]);
+  });
+
+  it("should have enum of resource names", () => {
+    expect(ResourceTypes).toHaveProperty("CICSProgram");
+    expect(ResourceTypes.CICSProgram).toEqual("CICSProgram");
+  });
+});
