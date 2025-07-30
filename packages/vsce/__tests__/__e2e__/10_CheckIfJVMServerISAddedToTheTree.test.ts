4ae65343f45f8dbb618f0b84650f18ad33fa68ca
Harshitha J <harshitha.j2@ibm.com>
2025-07-30 17:39:58 +0530

    Removed config file
    Signed-off-by: Harshitha J <harshitha.j2@ibm.com>

--- packages/vsce/__tests__/__e2e__/10_CheckIfJVMServerISAddedToTheTree.test.ts
+++ packages/vsce/__tests__/__e2e__/10_CheckIfJVMServerISAddedToTheTree.test.ts
@@ -1,30 +1,42 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
 import { expect } from "chai";
-import { DefaultTreeSection, TreeItem } from "vscode-extension-tester";
-import { WIREMOCK_PROFILE_NAME, CICSEX61, REGIONS, REGIONS_LOADED } from "./util/constants";
+import { DefaultTreeSection, TreeItem} from "vscode-extension-tester";
+import { EYUCMCIJ, CICSEX61, IYCWENW2, REGIONS, WIREMOCK_PROFILE_NAME, JVMSERVERS } from "./util/constants";
 import {
+  findJVMServerTreeNodeByLabel,
   sleep,
 } from "./util/globalMocks";
 import {
+  clickCollapseAllsIconInCicsTree,
+  closeAllEditorsTabs,
   getCicsSection,
   getPlexChildIndex,
   getPlexChildren,
-  getRegionIndex,
-  getRegionResourceIndex,
-  getRegionsInPlex,
+  setupCICSTreeSelectedResourceParams,
   openZoweExplorer,
 } from "./util/initSetup.test";
-import { resetAllScenarios } from "./util/resetScenarios";
 
-describe("Check if JVM server is in the tree", () => {
+describe("JVM Servers", () => {
   let cicsTree: DefaultTreeSection;
-  let regions: TreeItem[];
-  let regionWithJVMServerIndex: number;
+  let JVMServers: TreeItem[];
+  let regionResources: TreeItem[];
+  let regionIYCWENW2Index: number;
+  let JVMServerResourceIndex: number;
 
   before(async () => {
     await sleep(1900);
     const view = await openZoweExplorer();
     cicsTree = await getCicsSection(view);
-    await sleep(1900);
 
     const wiremockServer = await cicsTree.findItem(WIREMOCK_PROFILE_NAME);
     expect(wiremockServer).exist;
@@ -32,10 +44,12 @@ describe("Check if JVM server is in the tree", () => {
   });
 
   after(async () => {
-    await resetAllScenarios();
+    await closeAllEditorsTabs();
+    await clickCollapseAllsIconInCicsTree(cicsTree);
   });
 
   describe("Checking Children of CICSEX61", () => {
+    let JVMServerNode: TreeItem | undefined;
     it("Verify CICSEX61 -> Regions", async () => {
       const cicsex61Children = await getPlexChildren(cicsTree, WIREMOCK_PROFILE_NAME, CICSEX61);
       expect(cicsex61Children).not.empty;
@@ -45,24 +59,20 @@ describe("Check if JVM server is in the tree", () => {
       cicsTree.takeScreenshot();
     });
 
-    it("Verify CICSEX61 -> Regions -> IYCWENW2", async () => {
-      regions = await getRegionsInPlex(cicsTree, WIREMOCK_PROFILE_NAME, CICSEX61);
-      expect(regions).not.empty;
-
-      regionWithJVMServerIndex = await getRegionIndex(regions, "IYCWENW2");
-      expect(regionWithJVMServerIndex).to.be.greaterThan(-1);
+    it("Verify CICSEX61 -> Regions -> IYCWENW2 -> JVMServers", async () => {
+      ({
+        selectedRegionIndex: regionIYCWENW2Index,
+        selectedRegionResources: regionResources,
+        selectedResourceIndex: JVMServerResourceIndex,
+        selectedResource: JVMServers,
+      } = await setupCICSTreeSelectedResourceParams(cicsTree, WIREMOCK_PROFILE_NAME, CICSEX61, IYCWENW2, JVMSERVERS));
       cicsTree.takeScreenshot();
     });
-
-    it("Verify CICSEX61 -> Regions -> IYCWENW2 -> JVM Servers", async () => {
-      const regionResources = await cicsTree.openItem(WIREMOCK_PROFILE_NAME, CICSEX61, REGIONS_LOADED, await regions[regionWithJVMServerIndex].getLabel());
-      expect(regionResources).not.empty;
-
-      const jvmServersResourceIndex = await getRegionResourceIndex(regionResources, "JVM Servers");
-      expect(jvmServersResourceIndex).to.be.greaterThan(-1);
-      cicsTree.takeScreenshot();
-    });
-  }); 
-
-}); 
-
+     it("Verify JVMSERVERS -> EYUCMCIJ", async () => {
+       const JVMServerNode = await findJVMServerTreeNodeByLabel(JVMServers, EYUCMCIJ);
+       expect(JVMServerNode).not.undefined;
+       expect(await JVMServerNode?.getLabel()).contains(EYUCMCIJ);
+       cicsTree.takeScreenshot();
+     });
+     });   
+   }); 
