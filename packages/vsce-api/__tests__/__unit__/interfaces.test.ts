511687fcc8a7b3417536dee926897b04d0b1ffb8
Andrew Twydell <andrew.twydell@ibm.com>
2025-08-06 12:23:18 +0100

    register actions with resourceExtender
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce-api/__tests__/__unit__/interfaces.test.ts
+++ packages/vsce-api/__tests__/__unit__/interfaces.test.ts
@@ -9,11 +9,40 @@
  *
  */
 
+import { CICSSession } from "@zowe/cics-for-zowe-sdk";
+import { imperative } from "@zowe/zowe-explorer-api";
 import { IExtensionAPI } from "../../src/interfaces/IExtensionAPI";
 import { IResource } from "../../src/interfaces/IResource";
-import { SupportedResourceTypes } from "../../src/resources";
+import { IResourceContext } from "../../src/interfaces/IResourceContext";
+import { IResourceExtender } from "../../src/interfaces/IResourceExtender";
+import { ResourceTypes, SupportedResourceTypes } from "../../src/resources";
+import { IResourceAction } from "../../src/interfaces/IResourceAction";
 
 describe("Interfaces", () => {
+  const action: IResourceAction = {
+    id: "CICS.CICSProgram.NEWCOPY",
+    name: "New Copy Program",
+    resourceType: ResourceTypes.CICSProgram,
+    action: async (_resource: IResource, _resourceContext: IResourceContext) => { },
+    enabledWhen(_resource, _resourceContext) {
+      return true;
+    },
+    visibleWhen(_resource, _resourceContext) {
+      return true;
+    },
+  };
+
+  const extender: IResourceExtender = {
+    registeredActions: [],
+    deregisterAction(_id) { },
+    registerAction(_action) { },
+    getAction(_id) {
+      return action;
+    },
+    getActions() {
+      return [];
+    },
+  };
 
   const api: IExtensionAPI = {
     resources: {
@@ -26,6 +55,42 @@ describe("Interfaces", () => {
     status: "ENABLED",
   };
 
+  const profile: imperative.IProfileLoaded = {
+    failNotFound: false,
+    message: "",
+    type: "cics",
+    name: "MYPROF",
+    profile: {
+      host: "MYHOST",
+      port: 1234,
+    },
+  };
+
+  // @ts-ignore - profile will not be undefined
+  const session: CICSSession = new CICSSession(profile.profile);
+
+  const cx: IResourceContext = {
+    profile,
+    cicsplexName: "myplex",
+    regionName: "REGION1",
+    session,
+  };
+
+  it("should assert IResourceAction", () => {
+    expect(action).toHaveProperty("id");
+    expect(action).toHaveProperty("name");
+    expect(action).toHaveProperty("resourceType");
+    expect(action).toHaveProperty("action");
+    expect(action).toHaveProperty("enabledWhen");
+    expect(action).toHaveProperty("visibleWhen");
+  });
+  it("should assert IResourceExtender", () => {
+    expect(extender).toHaveProperty("registeredActions");
+    expect(extender).toHaveProperty("deregisterAction");
+    expect(extender).toHaveProperty("registerAction");
+    expect(extender).toHaveProperty("getAction");
+    expect(extender).toHaveProperty("getActions");
+  });
   it("should assert IExtensionAPI", () => {
     expect(api).toHaveProperty("resources");
     expect(api.resources).toHaveProperty("supportedResources");
@@ -33,5 +98,11 @@ describe("Interfaces", () => {
   it("should assert IResource", () => {
     expect(res).toHaveProperty("eyu_cicsname");
     expect(res).toHaveProperty("status");
+  });
+  it("should assert IResourceContext", () => {
+    expect(cx).toHaveProperty("session");
+    expect(cx).toHaveProperty("profile");
+    expect(cx).toHaveProperty("cicsplexName");
+    expect(cx).toHaveProperty("regionName");
   });
 });
