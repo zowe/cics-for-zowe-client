38a55caad277fa6caff3d65c99d7b2cd6a522a72
Andrew Twydell <andrew.twydell@ibm.com>
2025-08-21 13:50:08 +0100

    only delete file if exists
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/__tests__/__e2e__/setup.sh
+++ packages/vsce/__tests__/__e2e__/setup.sh
@@ -2,7 +2,11 @@
 install-extension /config/workspace/dist/cics-extension-for-zowe-3.*.vsix --extensions-dir /config/extensions
 
 mkdir -p /config/data/User
-rm /config/data/User/settings.json || true
+
+if [ -f "/config/data/User/settings.json" ]; then
+  rm /config/data/User/settings.json
+fi
+
 cp /config/workspace/globalUserSettings.json /config/data/User/settings.json
 chmod -R 777 /config/data
 
