76b5345df4f387afcb5f726bda9d0b5c71911a98
Harshitha J <harshitha.j2@ibm.com>
2025-09-10 15:37:46 +0530

    Added Constants
    Signed-off-by: Harshitha J <harshitha.j2@ibm.com>

---
+++ packages/vsce/__tests__/__unit__/doc/meta.jvmendpoint.test.ts
@@ -0,0 +1,73 @@
+import { JVMEndpointMeta } from "../../../src/doc/meta/jvmEndpoints.meta";
+import { IJVMEndpoint } from "../../../src/doc/resources/IJVMEndpoint";
+import { IJVMServer } from "../../../src/doc/resources/IJVMServer";
+import { Resource } from "../../../src/resources";
+
+describe("Bundle Part Meta", () => {
+  let jvmEndpointMock: Resource<IJVMEndpoint>;
+  let parentResource: Resource<IJVMServer>;
+
+  beforeEach(() => {
+    parentResource = new Resource<IJVMServer>({
+      eyu_cicsname: "MYREG",
+      name: "JVM1",
+      status: "ENABLED",
+      enablestatus: "ENABLED",
+    });
+    jvmEndpointMock = new Resource({
+      eyu_cicsname: "MYREG",
+      name: "JVME1",
+      status: "ENABLED",
+      enablestatus: "ENABLED",
+      jvmendpoint: "JVME1", 
+      jvmserver: "JVM1",
+    });
+  });
+
+  it("should return icon name", () => {
+    const iconName = JVMEndpointMeta.getIconName(jvmEndpointMock);
+    expect(iconName).toEqual(`jvm-server-endpoint`);
+  });
+  it("should build criteria", () => {
+      const crit = JVMEndpointMeta.buildCriteria(["a", "b"]);
+      expect(crit).toEqual(`name=a OR name=b`);
+    });
+    it("should return only label if enabled", () => {
+      const label = JVMEndpointMeta.getLabel(jvmEndpointMock);
+      expect(label).toEqual(`JVME1`);
+    });
+    it("should return label with disabled", () => {
+      jvmEndpointMock.attributes.enablestatus = "DISABLED";
+      const label = JVMEndpointMeta.getLabel(jvmEndpointMock);
+      expect(label).toEqual(`JVME1 (Disabled)`);
+    });
+    it("should return context with enabled status when enabled", () => {
+      jvmEndpointMock.attributes.enablestatus = "ENABLED";
+      const context = JVMEndpointMeta.getContext(jvmEndpointMock);
+      expect(context).toEqual(`CICSJVMServer.ENABLED.JVME1`);
+    });
+    it("should return context with disabled status when disabled", () => {
+      jvmEndpointMock.attributes.enablestatus = "DISABLED";
+      const context = JVMEndpointMeta.getContext(jvmEndpointMock);
+      expect(context).toEqual(`CICSJVMServer.DISABLED.JVME1`);
+    });
+    it("should get name", () => {
+        const name = JVMEndpointMeta.getName(jvmEndpointMock);
+        expect(name).toEqual("JVME1");
+    });
+  
+    it("should append criteria history", async () => {
+      const criteria = "JVME1";
+      await JVMEndpointMeta.appendCriteriaHistory(criteria);
+      let history = JVMEndpointMeta.getCriteriaHistory();
+      expect(history).toEqual(["JVME1"]);
+    });
+  
+    it("should get criteria history", async () => {
+      const criteria = "JVME1";
+      await JVMEndpointMeta.appendCriteriaHistory(criteria);
+      let history = JVMEndpointMeta.getCriteriaHistory();
+      expect(history).toEqual(["JVME1"]);
+    });
+    });
+    
