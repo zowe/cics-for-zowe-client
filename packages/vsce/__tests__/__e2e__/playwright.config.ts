d7f179d51590c90596f2a7618e271c26c6fed16a
Andrew Twydell <andrew.twydell@ibm.com>
2025-09-10 15:32:40 +0100

    tidy playwright config, rename e2e scripts
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/__tests__/__e2e__/playwright.config.ts
+++ packages/vsce/__tests__/__e2e__/playwright.config.ts
@@ -1,25 +1,14 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./specs/",
   fullyParallel: false,
   reporter: "list",
-  // reporter: 'html',
-  retries: 1,
-  // use: {
-  //   trace: 'on-first-retry',
-  // },
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  retries: 2,
 
   projects: [
     {
@@ -27,11 +16,4 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
