d6def2be466316592e1bc27ad5fc618a422a36cf
Harshitha J <harshitha.j2@ibm.com>
2025-09-19 10:09:49 +0530

    Added E2E testcases
    Signed-off-by: Harshitha J <harshitha.j2@ibm.com>

---
+++ packages/vsce/__tests__/__e2e__/specs/jvmEndpoint.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "@playwright/test";
+import { constants, findAndClickTreeItem, getTreeItem, prepareZoweExplorerView, resetWiremock, resetZoweExplorerView } from "../utils/helpers";
+
+test.beforeEach(async ({ page, request }) => {
+  await resetWiremock(request);
+  await prepareZoweExplorerView(page);
+});
+
+test.afterEach(async ({ page }) => {
+  await resetZoweExplorerView(page);
+});
+
+test.describe("JVM Endpoint tests", () => {
+  test("should expand JVM server tree to reveal JVM endpoint", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await findAndClickTreeItem(page, constants.REGION_NAME);
+    await findAndClickTreeItem(page, constants.JVM_SERVER_1_NAME);
+    await findAndClickTreeItem(page, "JVM Endpoints");
+
+    await expect(getTreeItem(page, "defaultHttpEndpoint")).toBeVisible();
+    await expect(getTreeItem(page, "defaultHttpEndpoint")).toHaveText(constants.JVM_SERVER_1_NAME);
+  });
+});
