ed73c76895463b0ac994e0b6c5174e01781f045a
Andrew Twydell <andrew.twydell@ibm.com>
2025-10-23 11:58:44 +0100

    send parent resource, adjust e2e criteria
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/src/commands/enableCommands/enableResourceCommand.ts
+++ packages/vsce/src/commands/enableCommands/enableResourceCommand.ts
@@ -61,7 +61,12 @@ export const enableTreeItem = async (
           nodesToRefresh.add(node.getParent());
 
           if (pollCriteria) {
-            await pollForCompleteAction(node, pollCriteria, (_response: ICMCIApiResponse) => evaluateTreeNodes(node, _response, node.getContainedResource().meta));
+            await pollForCompleteAction(
+              node,
+              pollCriteria,
+              (_response: ICMCIApiResponse) => evaluateTreeNodes(node, _response, node.getContainedResource().meta),
+              getParentResource(node.getParent() as CICSResourceContainerNode<IResource>)
+            );
           } else {
             evaluateTreeNodes(node, response, node.getContainedResource().meta);
           }
