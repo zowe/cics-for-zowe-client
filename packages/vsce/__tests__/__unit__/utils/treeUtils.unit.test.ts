6c9f127f8deed1920d3313cdc6dc1f5b30fa2c7d
Andrew Twydell <andrew.twydell@ibm.com>
2025-10-28 23:40:34 +0000

    add treeutils units
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

---
+++ packages/vsce/__tests__/__unit__/utils/treeUtils.unit.test.ts
@@ -0,0 +1,123 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { CICSRegionTree, CICSResourceContainerNode, CICSSessionTree, CICSTree } from "../../../src/trees";
+import { evaluateTreeNodes } from "../../../src/utils/treeUtils";
+import { ProgramMeta } from "../../../src/doc";
+import { IProfileLoaded } from "@zowe/imperative";
+import { ICMCIApiResponse } from "@zowe/cics-for-zowe-sdk";
+import PersistentStorage from "../../../src/utils/PersistentStorage";
+import { ResourceContainer } from "../../../src/resources";
+import { IProgram } from "@zowe/cics-for-zowe-explorer-api";
+
+jest.mock("../../../src/utils/profileManagement", () => ({
+  ProfileManagement: {},
+}));
+jest.spyOn(PersistentStorage, "getNumberOfResourcesToFetch").mockReturnValue(250);
+jest.spyOn(PersistentStorage, "getDefaultResourceFilter").mockReturnValue("Program=A*");
+
+const CICSProfileMock = {
+  host: "hostname",
+  port: "123",
+  user: "a",
+  password: "b",
+  rejectUnauthorized: false,
+  protocol: "http",
+};
+const profile: IProfileLoaded = { profile: CICSProfileMock, failNotFound: false, message: "", type: "cics", name: "MYPROF" };
+
+const cicsTree = { _onDidChangeTreeData: { fire: () => jest.fn() }, refresh: () => { } } as unknown as CICSTree;
+
+const sessionTree = new CICSSessionTree(profile, cicsTree);
+const regionTree = new CICSRegionTree("REG", {}, sessionTree, undefined, sessionTree);
+const parentNode = new CICSResourceContainerNode(
+  "Programs",
+  {
+    parentNode: regionTree,
+    profile,
+    regionName: "REG",
+    cicsplexName: ""
+  },
+  undefined,
+  {
+    meta: ProgramMeta,
+    resources: new ResourceContainer(ProgramMeta),
+  }
+);
+const prog: IProgram = {
+  eyu_cicsname: "REG",
+  library: "",
+  librarydsn: "",
+  newcopycnt: "1",
+  program: "APROG",
+  progtype: "",
+  status: "ENABLED"
+};
+
+describe("Tree Utils tests", () => {
+
+  let resourceNode: CICSResourceContainerNode<IProgram>;
+
+  beforeEach(() => {
+    resourceNode = new CICSResourceContainerNode(
+      "APROG1",
+      {
+        parentNode,
+        profile,
+        regionName: "REG",
+        cicsplexName: ""
+      },
+      {
+        meta: ProgramMeta,
+        resource: { attributes: prog }
+      },
+      {
+        meta: ProgramMeta,
+        resources: new ResourceContainer(ProgramMeta),
+      }
+    );
+
+    jest.resetAllMocks();
+  });
+
+  it("should do nothing if no record is returned", () => {
+
+    const apiResp: ICMCIApiResponse = {
+      response: {
+        records: [],
+        resultsummary: { api_response1: "", api_response2: "", displayed_recordcount: "0", recordcount: "0" }
+      }
+    };
+    const updateItemSpy = jest.spyOn(CICSResourceContainerNode.prototype, "setContainedResource");
+    evaluateTreeNodes(resourceNode, apiResp, ProgramMeta);
+
+    expect(updateItemSpy).not.toHaveBeenCalled();
+  });
+
+  it("should update the record in the resource node", () => {
+
+    const updatedProgram = { ...prog, newcopycnt: 2 };
+    const apiResp: ICMCIApiResponse = {
+      response: {
+        records: {
+          cicsprogram: updatedProgram
+        },
+        resultsummary: { api_response1: "", api_response2: "", displayed_recordcount: "0", recordcount: "0" }
+      }
+    };
+    const updateItemSpy = jest.spyOn(CICSResourceContainerNode.prototype, "setContainedResource");
+    evaluateTreeNodes(resourceNode, apiResp, ProgramMeta);
+
+    expect(updateItemSpy).toHaveBeenCalledTimes(1);
+    expect(updateItemSpy).toHaveBeenCalledWith({ attributes: updatedProgram });
+  });
+
+});
