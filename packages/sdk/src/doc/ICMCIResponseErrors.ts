726a9e8d168300fb0c3866247469785f4ccff270
Rushabh Sojitra <Rushabh.Sojitra@ibm.com>
2025-10-30 15:28:27 +0530

    Reapply "Added changelog" This reverts commit c285e0c.
    This reverts commit 30fdbc80e099ca8b3173da7a083efd4613e70bcc.
    
    Signed-off-by: Rushabh Sojitra <Rushabh.Sojitra@ibm.com>

---
+++ packages/sdk/src/doc/ICMCIResponseErrors.ts
@@ -0,0 +1,23 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { ICMCIResponseErrorFeedBack } from "./ICMCIResponseErrorFeedBack";
+
+/**
+ * Represents the "errors" field of the CMCI API response, parsed from XML
+ * to a javascript object with the xml2js package.
+ * See the following link for more information:
+ * https://www.ibm.com/support/knowledgecenter/SSGMCP_5.2.0/com.ibm.cics.ts.clientapi.doc/topics/clientapi_errors_element.html
+ * 
+ */
+export interface ICMCIResponseErrors {
+  feedback: ICMCIResponseErrorFeedBack
+}
