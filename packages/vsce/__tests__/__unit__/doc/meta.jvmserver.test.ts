0b3121120e44e398832172e5247c730fed73df41
Harshitha J <harshitha.j2@ibm.com>
2025-11-25 22:23:22 +0530

    Updating e2e Files meta.jvmserver.test.ts
    Signed-off-by: Harshitha J <harshitha.j2@ibm.com>

--- packages/vsce/__tests__/__unit__/doc/meta.jvmserver.test.ts
+++ packages/vsce/__tests__/__unit__/doc/meta.jvmserver.test.ts
@@ -33,45 +33,44 @@ describe("JVMServer Meta", () => {
     expect(iconName).toEqual(`jvm-server`);
   });
   it("should build criteria", () => {
-      const crit = JVMServerMeta.buildCriteria(["a", "b"]);
-      expect(crit).toEqual(`name=a OR name=b`);
-    });
-    it("should return only label if enabled", () => {
-      const label = JVMServerMeta.getLabel(jvmserverMock);
-      expect(label).toEqual(`JVM1`);
-    });
-    it("should return label with disabled", () => {
-      jvmserverMock.attributes.enablestatus = "DISABLED";
-      const label = JVMServerMeta.getLabel(jvmserverMock);
-      expect(label).toEqual(`JVM1 (Disabled)`);
-    });
-    it("should return context with enabled status when enabled", () => {
-      jvmserverMock.attributes.enablestatus = "ENABLED";
-      const context = JVMServerMeta.getContext(jvmserverMock);
-      expect(context).toEqual(`CICSJVMServer.ENABLED.JVM1`);
-    });
-    it("should return context with disabled status when disabled", () => {
-      jvmserverMock.attributes.enablestatus = "DISABLED";
-      const context = JVMServerMeta.getContext(jvmserverMock);
-      expect(context).toEqual(`CICSJVMServer.DISABLED.JVM1`);
-    });
-    it("should get name", () => {
-        const name = JVMServerMeta.getName(jvmserverMock);
-        expect(name).toEqual("JVM1");
-      });
-  
-    it("should append criteria history", async () => {
-      const criteria = "JVM1";
-      await JVMServerMeta.appendCriteriaHistory(criteria);
-      let history = JVMServerMeta.getCriteriaHistory();
-      expect(history).toEqual(["JVM1"]);
-    });
-  
-    it("should get criteria history", async () => {
-      const criteria = "JVM1";
-      await JVMServerMeta.appendCriteriaHistory(criteria);
-      let history = JVMServerMeta.getCriteriaHistory();
-      expect(history).toEqual(["JVM1"]);
-    });
-    });
-    
+    const crit = JVMServerMeta.buildCriteria(["a", "b"]);
+    expect(crit).toEqual(`name=a OR name=b`);
+  });
+  it("should return only label if enabled", () => {
+    const label = JVMServerMeta.getLabel(jvmserverMock);
+    expect(label).toEqual(`JVM1`);
+  });
+  it("should return label with disabled", () => {
+    jvmserverMock.attributes.enablestatus = "DISABLED";
+    const label = JVMServerMeta.getLabel(jvmserverMock);
+    expect(label).toEqual(`JVM1 (DISABLED)`);
+  });
+  it("should return context with enabled status when enabled", () => {
+    jvmserverMock.attributes.enablestatus = "ENABLED";
+    const context = JVMServerMeta.getContext(jvmserverMock);
+    expect(context).toEqual(`CICSJVMServer.ENABLED.JVM1`);
+  });
+  it("should return context with disabled status when disabled", () => {
+    jvmserverMock.attributes.enablestatus = "DISABLED";
+    const context = JVMServerMeta.getContext(jvmserverMock);
+    expect(context).toEqual(`CICSJVMServer.DISABLED.JVM1`);
+  });
+  it("should get name", () => {
+    const name = JVMServerMeta.getName(jvmserverMock);
+    expect(name).toEqual("JVM1");
+  });
+
+  it("should append criteria history", async () => {
+    const criteria = "JVM1";
+    await JVMServerMeta.appendCriteriaHistory(criteria);
+    let history = JVMServerMeta.getCriteriaHistory();
+    expect(history).toEqual(["JVM1"]);
+  });
+
+  it("should get criteria history", async () => {
+    const criteria = "JVM1";
+    await JVMServerMeta.appendCriteriaHistory(criteria);
+    let history = JVMServerMeta.getCriteriaHistory();
+    expect(history).toEqual(["JVM1"]);
+  });
+});
