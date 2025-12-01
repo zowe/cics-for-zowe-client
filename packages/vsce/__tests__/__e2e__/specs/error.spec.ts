9e73c00661ef8020cf28141b5b45efe0ac127af0
Rushabh Sojitra <Rushabh.Sojitra@ibm.com>
2025-12-01 19:10:50 +0530

    e2e test cases for error scenarios
    Signed-off-by: Rushabh Sojitra <Rushabh.Sojitra@ibm.com>

---
+++ packages/vsce/__tests__/__e2e__/specs/error.spec.ts
@@ -0,0 +1,97 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import { expect, test } from "@playwright/test";
+import {
+  constants,
+  findAndClickText,
+  findAndClickTreeItem,
+  getTreeItem,
+  prepareZoweExplorerView,
+  resetWiremock,
+  resetZoweExplorerView,
+} from "../utils/helpers";
+
+test.beforeEach(async ({ page, request }) => {
+  await resetWiremock(request);
+  await prepareZoweExplorerView(page);
+});
+
+test.afterEach(async ({ page }) => {
+  await resetZoweExplorerView(page);
+});
+
+test.describe("Error scenarios", () => {
+  test("jvm server throws error when killing it", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await findAndClickTreeItem(page, constants.REGION_ERROR);
+    await findAndClickTreeItem(page, "JVM Servers");
+
+    await expect(getTreeItem(page, constants.JVM_SERVER_1_NAME)).toBeVisible();
+    await expect(getTreeItem(page, constants.JVM_SERVER_1_NAME)).toHaveText(constants.JVM_SERVER_1_NAME);
+
+    //Disable action from context menu
+    await expect(getTreeItem(page, constants.JVM_SERVER_1_NAME)).toBeVisible();
+    await getTreeItem(page, constants.JVM_SERVER_1_NAME).click({ button: "right" });
+
+    await page.waitForTimeout(200);
+    await findAndClickText(page, "Disable JVM Server");
+    //Click on Kill button
+    await expect(page.getByRole("button", { name: "Kill", exact: true })).toBeVisible();
+    await page.getByRole("button", { name: "Kill", exact: true }).click();
+
+    const notification = page.getByRole("list", { name: "The CMCI REST API request failed", exact: false });
+    await expect(notification).toBeVisible();
+    await expect(notification).toHaveText(constants.JVM_SERVER_DISABLE_ERROR_MESSAGE);
+  });
+
+  test("enabling bundle throws error", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await findAndClickTreeItem(page, constants.REGION_ERROR);
+    await findAndClickTreeItem(page, "Bundles");
+
+    await expect(getTreeItem(page, constants.BUNDLE_1_NAME)).toBeVisible();
+    await expect(getTreeItem(page, constants.BUNDLE_1_NAME)).toHaveText(constants.BUNDLE_1_NAME);
+
+    await expect(getTreeItem(page, constants.BUNDLE_1_NAME)).toBeVisible();
+    await getTreeItem(page, constants.BUNDLE_1_NAME).click({ button: "right" });
+
+    await page.waitForTimeout(200);
+    await findAndClickText(page, constants.ENABLE_BUNDLE);
+
+    const notification = page.getByRole("list", { name: "The CMCI REST API request failed", exact: false });
+    await expect(notification).toBeVisible();
+    await expect(notification).toHaveText(constants.BUNDLE_ENABLE_ERROR_MESSAGE);
+  });
+
+  test("invalid filter in transaction throws error", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await findAndClickTreeItem(page, constants.REGION_ERROR);
+    await findAndClickTreeItem(page, "Transactions");
+
+    const filterButton = page.getByRole("button", { name: "Filter Resources", exact: true });
+    await expect(filterButton).toBeVisible();
+    await filterButton.click();
+
+    const textBox = page.getByRole("textbox", { name: "Select a Filter", exact: true });
+    await expect(textBox).toBeEditable();
+    await textBox.fill("FILTER");
+    await textBox.press("Enter");
+    await page.getByRole("textbox", { name: "input", exact: true }).press("Enter");
+
+    const notification = page.getByRole("list", { name: "The CMCI REST API request failed", exact: false });
+    await expect(notification).toBeVisible();
+    await expect(notification).toHaveText(constants.TRANSACTION_INVALID_FILTER_ERROR_MESSAGE);
+  });
+});
