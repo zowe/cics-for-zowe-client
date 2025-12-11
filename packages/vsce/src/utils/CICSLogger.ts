d204ef58814f98da2d183a07374da54fa080d079
Andrew Twydell <andrew.twydell@ibm.com>
2025-12-11 14:31:36 +0000

    make cicslogger unit testable
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/src/utils/CICSLogger.ts
+++ packages/vsce/src/utils/CICSLogger.ts
@@ -14,17 +14,18 @@
 import { extensions, l10n, LogLevel, LogOutputChannel, window } from "vscode";
 
 export class CICSLogger {
+  private static amInitialized = false;
   private static logOutputChannel: LogOutputChannel;
 
-  public static initialize() {
+  private static initialize() {
     CICSLogger.logOutputChannel = window.createOutputChannel(l10n.t("Zowe Explorer for IBM CICS TS"), { log: true });
 
-    CICSLogger.info(l10n.t("Initialized logger for Zowe Explorer for IBM CICS TS"));
+    CICSLogger.logOutputChannel.info(l10n.t("Initialized logger for Zowe Explorer for IBM CICS TS"));
 
     const packageJSON = extensions.getExtension("zowe.cics-extension-for-zowe").packageJSON;
 
-    CICSLogger.info(`${packageJSON.displayName as string} ${packageJSON.version as string}`);
-    CICSLogger.info(
+    CICSLogger.logOutputChannel.info(`${packageJSON.displayName as string} ${packageJSON.version as string}`);
+    CICSLogger.logOutputChannel.info(
       l10n.t({
         message: "Zowe Explorer for IBM CICS TS log level: {0}",
         args: [LogLevel[this.logOutputChannel.logLevel]],
@@ -33,31 +34,45 @@ export class CICSLogger {
     );
   }
 
+  private static ensureInitialized(): void {
+    if (!CICSLogger.amInitialized) {
+      CICSLogger.initialize();
+      CICSLogger.amInitialized = true;
+    }
+  }
+
   public static trace(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.trace(message);
   }
 
   public static debug(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.debug(message);
   }
 
   public static info(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.info(message);
   }
 
   public static warn(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.warn(message);
   }
 
   public static error(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.error(message);
   }
 
   public static fatal(message: string): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.error(message);
   }
 
   public static dispose(): void {
+    CICSLogger.ensureInitialized();
     CICSLogger.logOutputChannel.dispose();
   }
 }
