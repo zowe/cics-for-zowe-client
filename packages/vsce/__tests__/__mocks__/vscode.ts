a87f447b892904c87efa5a0739d1b57061578403
Andrew Twydell <andrew.twydell@ibm.com>
2025-12-11 14:31:05 +0000

    add global mocks with jest
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- packages/vsce/__tests__/__mocks__/vscode.ts
+++ packages/vsce/__tests__/__mocks__/vscode.ts
@@ -10,13 +10,26 @@
  */
 
 import { createVSCodeMock } from "jest-mock-vscode";
+import {
+  showErrorMessageMock,
+  showInfoMessageMock,
+  vscodeExecuteCommandMock,
+  vscodeRegisterCommandMock,
+  workspaceConfigurationGetMock,
+  workspaceConfigurationUpdateMock,
+} from ".";
 
 const mock = createVSCodeMock(jest);
 
 module.exports = {
   ...mock,
   extensions: {
-    getExtension: (v: string) => {},
+    getExtension: jest.fn().mockReturnValue({
+      packageJSON: {
+        displayName: "Zowe Explorer for IBM CICS TS",
+        version: "3.15.0",
+      },
+    }),
   },
   env: {
     clipboard: {
@@ -30,5 +43,57 @@ module.exports = {
       }
       return key.replace(/\{(\d+)\}/g, (_, idx) => (args[Number(idx)] !== undefined ? String(args[Number(idx)]) : ""));
     },
+  },
+  window: {
+    ...mock.window,
+    createOutputChannel: jest.fn().mockReturnValue({
+      trace: jest.fn(),
+      debug: jest.fn(),
+      info: jest.fn(),
+      warn: jest.fn(),
+      error: jest.fn(),
+      dispose: jest.fn(),
+      logLevel: 2,
+    }),
+    createTreeView: jest.fn().mockReturnValue({
+      onDidExpandElement: jest.fn(),
+      onDidCollapseElement: jest.fn(),
+      reveal: jest.fn(),
+    }),
+    showErrorMessage: showErrorMessageMock,
+    showInformationMessage: showInfoMessageMock,
+  },
+  commands: {
+    ...mock.commands,
+    registerCommand: vscodeRegisterCommandMock,
+    executeCommand: vscodeExecuteCommandMock,
+  },
+  workspace: {
+    ...mock.workspace,
+    getConfiguration: jest.fn().mockImplementation((schema: string) => {
+      switch (schema) {
+        case "zowe.cics.resources":
+          const resMap = new Map();
+          resMap.set("Program", true);
+          resMap.set("Transaction", false);
+          resMap.set("LocalFile", false);
+          resMap.set("Task", true);
+          resMap.set("Library", true);
+          resMap.set("Pipeline", true);
+          resMap.set("TCP/IPService", true);
+          resMap.set("URIMap", true);
+          resMap.set("WebService", true);
+          resMap.set("JVMServer", true);
+          resMap.set("Bundle", true);
+          resMap.set("TSQueue", true);
+
+          return resMap;
+        default:
+          return {
+            get: workspaceConfigurationGetMock,
+            update: workspaceConfigurationUpdateMock,
+          };
+      }
+    }),
   },
 };
