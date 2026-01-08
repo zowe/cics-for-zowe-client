05d84862118ba5220b8316c3a9f0b734de39eaaa
Harshitha J <harshitha.j2@ibm.com>
2026-01-08 23:49:44 +0530

    Added E2E
    Signed-off-by: Harshitha J <harshitha.j2@ibm.com>

--- packages/vsce/__tests__/__e2e__/specs/inspectRegion.spec.ts
+++ packages/vsce/__tests__/__e2e__/specs/inspectRegion.spec.ts
@@ -8,6 +8,7 @@ import {
   prepareZoweExplorerView,
   resetWiremock,
   resetZoweExplorerView,
+  waitForNotification,
 } from "../utils/helpers";
 
 test.beforeEach(async ({ page, request }) => {
@@ -18,8 +19,8 @@ test.beforeEach(async ({ page, request }) => {
 test.afterEach(async ({ page }) => {
   await resetZoweExplorerView(page);
 });
-test.describe("Resource Inspector tests", async () => {
-  test("should inspect a program resource", async ({ page }) => {
+test.describe("Inspect Region tests", async () => {
+  test("should inspect a region", async ({ page }) => {
     await findAndClickTreeItem(page, constants.PROFILE_NAME);
     await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
     await expect(getTreeItem(page, constants.REGION_NAME)).toBeVisible();
@@ -28,7 +29,34 @@ test.describe("Resource Inspector tests", async () => {
     await findAndClickText(page, "Inspect Region");
 
     await getResourceInspector(page).locator("#resource-title").waitFor();
-    await page.screenshot({ fullPage: true, path: "./__tests__/screenshots/resourceInspector/1.png" });
+    await page.screenshot({ fullPage: true, path: "./__tests__/screenshots/inspectRegion/1.png" });
     await expect(getResourceInspector(page).locator("th").first()).toHaveText(new RegExp(constants.REGION_NAME));
+  });
+  test("should have a filterable table", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await expect(getTreeItem(page, constants.REGION_NAME)).toBeVisible();
+    await findAndClickTreeItem(page, constants.REGION_NAME, "right", false);
+    await page.waitForTimeout(200);
+    await findAndClickText(page, "Inspect Region");
+
+    await getResourceInspector(page).locator("#resource-title").waitFor();
+    await expect(getResourceInspector(page).getByText("cedfstatus")).toBeDefined();
+
+    await getResourceInspector(page).locator("input").first().fill("name");
+    await page.screenshot({ fullPage: true, path: "./__tests__/screenshots/inspectRegion/2.png" });
+    await expect(getResourceInspector(page).locator("input").first()).toHaveValue("name");
+    await expect(getResourceInspector(page).locator("th").first()).toHaveText(new RegExp(constants.REGION_NAME));
+  });
+  test("should show loading message", async ({ page }) => {
+    await findAndClickTreeItem(page, constants.PROFILE_NAME);
+    await findAndClickTreeItem(page, constants.CICSPLEX_NAME);
+    await expect(getTreeItem(page, constants.REGION_NAME)).toBeVisible();
+    await findAndClickTreeItem(page, constants.REGION_NAME, "right", false);
+    await page.waitForTimeout(200);
+    await findAndClickText(page, "Inspect Region");
+
+    await waitForNotification(page, `Loading CICS resource '${constants.REGION_NAME}'...`);
+    await page.screenshot({ fullPage: true, path: "./__tests__/screenshots/inspectRegion/3.png" });
   });
 });
